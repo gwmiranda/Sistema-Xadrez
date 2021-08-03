a26080f2a6317bf3c6bde6f940367073cefd8286
Guilherme Miranda <guilherme.miranda@jallcard.com.br>
2021-08-03 11:45:18 -0300

    Jogada especial promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,13 @@ public class Program {
                 if(capturedPiece != null){
                     capured.add(capturedPiece);
                 }
+
+                if (chessMatch.getPromoted() != null){
+                    System.out.println("Enter piece for promotion (B/N/Q/R): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
+
             }catch (ChessException e){
                 System.out.println(e.getMessage());
                 sc.nextLine();
